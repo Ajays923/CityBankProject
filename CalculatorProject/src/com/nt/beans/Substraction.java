a8acdc28911767089d7f57af2894a221b02aca19
TL <TL@gmail.com>
2022-10-09 10:03:00 +0530

    comments in Substraction.java file

--- CalculatorProject/src/com/nt/beans/Substraction.java
+++ CalculatorProject/src/com/nt/beans/Substraction.java
@@ -1,12 +1,11 @@
 package com.nt.beans;
 
-import com.nt.logics.Multiplication;
-
 public class Substraction {
 
 	public static void main(String[] args) {
-		Multiplication ml = new Multiplication();	
-		ml.multiply(10, 5);
+		/*
+		 * Multiplication ml = new Multiplication(); ml.multiply(10, 5);
+		 */
 		System.out.println("Mulitplication Logic developed by Developer2");
 	}
 }
